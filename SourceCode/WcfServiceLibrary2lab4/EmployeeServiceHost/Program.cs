812219da28892cc34b78697f0c7f9b454601a604
Ayush Patel <64131425+ayush-2510@users.noreply.github.com>
2020-07-29 11:05:56 +0530

    Add files via upload

---
+++ SourceCode/WcfServiceLibrary2lab4/EmployeeServiceHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WcfServiceLibrary2lab4.UserService)))
+            {
+                using (ServiceHost host1 = new ServiceHost(typeof(WcfServiceLibrary2lab4.SlotService)))
+                {
+                    host1.Open();
+                    Console.WriteLine("host1 started @" + DateTime.Now.ToString());
+                    host.Open();
+                    Console.WriteLine("host2 started @" + DateTime.Now.ToString());
+                    Console.ReadLine();
+                }
+
+            }
+            /*
+            using (ServiceHost host = new ServiceHost(typeof(WcfServiceLibrary2lab4.UserService)))
+            {
+                host.Open();
+                Console.WriteLine("host started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }*/
+        }
+    }
+}
